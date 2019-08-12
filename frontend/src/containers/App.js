eee338bbabafc04173ef56412900b27995ea379a
Jairo Batista <jairo@g8jedi.com>
2019-08-12 12:25:11 -0400

    Initial commit

---
+++ frontend/src/containers/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import './App.css';
+import HomepageLayout from '../containers/Layout'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomepageLayout>
+
+        </HomepageLayout>
+      </header>
+    </div>
+  );
+}
+
+export default App;
